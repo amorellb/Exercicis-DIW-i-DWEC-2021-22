02d52c1da7aad5d4dbd41f7fcd2e30ac36149159
amorellb <amorellb@cifpfbmoll.eu>
2021-10-06 12:09:47 +0200

    feat: do exercise 4

--- DWEC/ec02/src/js/functions.js
+++ DWEC/ec02/src/js/functions.js
@@ -1,28 +1,32 @@
-import { clearResult } from "./helper.js";
+import { clearResult } from './helper.js';
 
 // Exercise 1
 export const isEmptyString = function (str, elem) {
   clearResult(elem);
-  if (!str || str === "" || str === null) return "Empty string";
+  if (!str || str === '' || str === null) return 'Empty string';
   return str;
 };
 
-// Exercise 2
+// Exercise 2 (split; reverse; join)
 export const toReverseString = function (str, elem) {
   clearResult(elem);
-  if (!str || str === "" || str === null) return "Empty string";
-  return str.split("").reverse().join("");
+  if (!str || str === '' || str === null) return 'Empty string';
+  return str.split('').reverse().join('');
 };
 
-// Exercise 3
+// Exercise 3 (split; forEach)
 export const countA = function (str, elem) {
   let count = 0;
   clearResult(elem);
-  if (!str || str === "" || str === null) return "Empty string";
-  str.split("").forEach((letter) => {
-    if (letter.toLowerCase() === "a") count++;
+  if (!str || str === '' || str === null) return 'Empty string';
+  str.split('').forEach(letter => {
+    if (letter.toLowerCase() === 'a') count++;
   });
   return count;
 };
 
-// Exercise 4
+// Exercise 4 (slice; indexOf)
+export const whatsBeforeA = function (str, elem) {
+  clearResult(elem);
+  return str.slice(0, str.toLowerCase().indexOf('a'));
+};
--- DWEC/ec02/src/js/index.js
+++ DWEC/ec02/src/js/index.js
@@ -1,36 +1,52 @@
-import { isEmptyString, toReverseString, countA } from "./functions.js";
-import { allInOne, addResultStyle } from "./helper.js";
+import * as Functions from './functions.js';
+import { allInOne, addResultStyle } from './helper.js';
 
 // const formElement = document.querySelector(".form");
-const inputString = document.querySelector("#inputString");
-const resultOutput = document.querySelector(".result");
+const inputString = document.querySelector('#inputString');
+const resultOutput = document.querySelector('.result');
 
 // Exercise 1
-const btnIsEmptyString = document.querySelector(".btnIsEmptyStringCheck");
+const btnIsEmptyString = document.querySelector('.btnIsEmptyStringCheck');
 // Exercise 2
-const btnToReverseString = document.querySelector(".btnReverseString");
+const btnToReverseString = document.querySelector('.btnReverseString');
 // Exercise 3
-const btnCountA = document.querySelector(".btnCountA");
+const btnCountA = document.querySelector('.btnCountA');
+// Exercise 4
+const btnBeforeA = document.querySelector('.btnBeforeA');
 
 // Exercise 1
-btnIsEmptyString.addEventListener("click", (e) => {
+btnIsEmptyString.addEventListener('click', e => {
   e.preventDefault();
   addResultStyle(resultOutput);
-  allInOne(resultOutput, isEmptyString(inputString.value, resultOutput));
+  allInOne(
+    resultOutput,
+    Functions.isEmptyString(inputString.value, resultOutput)
+  );
 });
 
 // Exercise 2
-btnToReverseString.addEventListener("click", (e) => {
+btnToReverseString.addEventListener('click', e => {
   e.preventDefault();
   addResultStyle(resultOutput);
-  allInOne(resultOutput, toReverseString(inputString.value, resultOutput));
+  allInOne(
+    resultOutput,
+    Functions.toReverseString(inputString.value, resultOutput)
+  );
 });
 
 // Exercise 3
-btnCountA.addEventListener("click", (e) => {
+btnCountA.addEventListener('click', e => {
   e.preventDefault();
   addResultStyle(resultOutput);
-  allInOne(resultOutput, countA(inputString.value, resultOutput));
+  allInOne(resultOutput, Functions.countA(inputString.value, resultOutput));
 });
 
 // Exercise 4
+btnBeforeA.addEventListener('click', e => {
+  e.preventDefault();
+  addResultStyle(resultOutput);
+  allInOne(
+    resultOutput,
+    Functions.whatsBeforeA(inputString.value, resultOutput)
+  );
+});
